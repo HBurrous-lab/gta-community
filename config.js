a85ef642dfdc23cbc32ac0134294287a4cc5fece
HBurrous-lab <burroushunter789@gmail.com>
2026-08-26 19:57:33 -0400

    Add safe Supabase configuration template

---
+++ config.js
@@ -0,0 +1,6 @@
+window.CH_CONFIG = {
+  // Add these values only in your deployed environment. Never use a Supabase service_role key here.
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  discordInvite: 'https://discord.gg/'
+};
